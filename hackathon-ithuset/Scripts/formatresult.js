c976ec564b90f316568990e20a823ce86b235bf8
lina-hs <lina.hannersanchez@gmail.com>
2017-03-11 17:33:31 +0100

    Added search-result formating

--- hackathon-ithuset/Scripts/formatresult.js
+++ hackathon-ithuset/Scripts/formatresult.js
@@ -3,21 +3,41 @@
 
 
 function formatResultDiv(resultObj) {
-       
-       var result = "<div class='search-result'>" + 
-                "<h3> Title: " + resultObj.title + "</h3>" +
-                "<h5> Creator: " + resultObj.creator + "</h5>" +
-                "<br>" + 
-                "<strong> Date: </strong>" + resultObj.date +
-                "<br>" + 
-                "<strong> Language: " + resultObj.language + 
-                "<br>";
 
-        if(resultObj.isbn != "undefined") {
-            result += "<a href='/Home/Book/" + resultObj.isbn + "'>View related books</a>"
-            }
-                     
-        result += "</div>"
+    var result = "<div class='search-result'>" +
+             "<h3> Title: " + resultObj.title + "</h3>" +
+             "<h5> Creator: " + resultObj.creator + "</h5>" +
+             "<br>" +
+             "<strong> Date: </strong>" + resultObj.date +
+             "<br>" +
+             "<strong> Language: " + resultObj.language +
+             "<br>";
 
-        return result;
+    if (resultObj.isbn) {
+        var isbn = resultObj.isbn;
+        if (isbn instanceof Array) {
+            isbn = isbn[0];
+        }
+        result += "<a href='/Home/Book/" + isbn + "'>View related books</a>"
+    }
+
+    result += "</div>"
+
+    return result;
+}
+
+function formatSearchResult(object) {
+    var result = "<div class='search-result'>" +
+        "<span class='title'>" + object.title + "</span>" +
+        "<span class='creator'>" + object.creator + "</span>";
+
+    if (object.isbn) {
+        var isbn = object.isbn;
+        if (isbn instanceof Array) {
+            isbn = isbn[0];
+        }
+        result += "<a href='/Home/Book/" + isbn + "'><i class='fa fa-external-link fa-1x'></i></a>"
+    }
+
+    return result;
 }
