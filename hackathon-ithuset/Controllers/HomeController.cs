cb2896929361b6e75cdae209c8e4327535ac6d27
lina-hs <lina.hannersanchez@gmail.com>
2017-03-11 10:46:48 +0100

    Added project start stuff

---
+++ hackathon-ithuset/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hackathon_ithuset.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View(((object)"Hej hej"));
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string query)
+        {
+            query = string.Format("http://libris.kb.se/xsearch?query={0}&format=json", query);
+
+            return View(((object)query));
+        }
+    }
+}
