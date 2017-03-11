8bc41446594cdc0177011117dafd6d61245e337a
lina-hs <lina.hannersanchez@gmail.com>
2017-03-11 16:28:09 +0100

    Sending related books to book view

--- hackathon-ithuset/Controllers/HomeController.cs
+++ hackathon-ithuset/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,9 +21,31 @@ namespace hackathon_ithuset.Controllers
         }
 
         // GET: Book
-        public ActionResult Book(int id = 0)
+        public ActionResult Book(string id)
         {
-            return View(id);
+            string html = string.Empty;
+            string url = "https://api.libris.kb.se/relrec/related?isbn=0-262-22029-6&format=json";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            JObject o = JObject.Parse(html);
+            JArray related = (JArray)o["relatedrecords"]["related"];
+
+            List<string> isbns = new List<string>();
+            foreach (JObject item in related)
+            {
+                isbns.Add((string)item["isbn"]);
+            }
+            
+            return View((object)isbns);
         }
     }
 }
---
+++ hackathon-ithuset/Models/BookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hackathon_ithuset.Models
+{
+    public class BookViewModel
+    {
+        string isbn;
+        List<string> relatedisbns;
+    }
+}
